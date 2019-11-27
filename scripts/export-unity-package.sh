b68eccde704bd7cee7c9401b19096f169b18e8f7
Hoch Hochkeppel <mhochk@microsoft.com>
2019-11-26 16:05:12 -0800

    Update export-unity-package.sh to accept arguments needed

--- scripts/export-unity-package.sh
+++ scripts/export-unity-package.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+unity_username=$1
+unity_password=$2
+unity_serial=$3
 project_path=$(pwd)/UnityGLTF
 log_file=$(pwd)/build/unity-mac.log
 export_path=$(pwd)/current-package/UnityGLTF.unitypackage
@@ -7,13 +10,20 @@ export_path=$(pwd)/current-package/UnityGLTF.unitypackage
 error_code=0
 
 echo "Creating package."
+# -username, -password, and -serial are all required to do the license activation from the
+# command line.  License activation is required in order to use -batchmode (without deceptive errors).
+# -nographics cannot be used with -serial
+# https://forum.unity.com/threads/package-manager-failed-to-resolve-packages-packages-directory-does-not-exist.522782/
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
-  -nographics \
   -silent-crashes \
+  -username "$unity_username" \
+  -password "$unity_password" \
+  -serial "$unity_serial" \
   -logFile "$log_file" \
   -projectPath "$project_path" \
   -exportPackage "Assets/UnityGLTF" "$export_path" \
+  -returnlicense \
   -quit
 if [ $? = 0 ] ; then
   echo "Created package successfully."
